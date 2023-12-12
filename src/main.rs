52d66a75f8a226cfe71a7b09439fb7be502f0522
Phillip Peterson <phillip.peterson873@gmail.com>
2023-12-12 12:28:25 -0800

    Added support for single non-variable atoms.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,48 @@
 use std::io;
-use std::collections::HashMap;
+//use std::collections::HashMap;
 
-fn eval(command: String, atable: &mut HashMap<String, String>) {
-    atable.insert(command, "value".to_string());
-    println!("{}", atable.get("ok").expect("Value not found.").to_string() );
+fn eval<'a>(command: String, alist: &mut Vec<String>) -> String {
+    let keyc = command.trim_end().to_string();
+    
+    // Eval on expressions
+    if keyc.starts_with('(') {
+
+    }
+    
+    // Eval on atoms
+    let result = eval_atom(keyc, alist);
+    return result;
 }
+
+
+fn eval_atom(atom: String, alist: &mut Vec<String>) -> String {
+    
+    // Check if the atom is a T, NIL, or a string.
+    let upper_check = atom.to_uppercase();
+    if upper_check == "T" || 
+        upper_check == "NIL" ||
+        atom.starts_with('\"') { return atom; }
+
+    // Check if the atom is numeric.
+    if let Ok(_num) = atom.parse::<f32>() {
+        return atom;
+    }
+    
+    return atom;
+}
+
 
 fn main() {
     // Declare association table as mutable hashmap.
-    let mut atable: HashMap<String, String> = HashMap::new();
+    let mut alist: Vec<String> = Vec::new();
     
     // Loop forever to continuously take commands.
     loop {
-        let mut command = String::new();
+        let mut command: String = String::new();
         match io::stdin().read_line(&mut command) {
             Ok(_) => {
-                eval(command, &mut atable);
+                let result: String = eval(command, &mut alist);
+                println!("{}", result);
             }
             Err(e) => {
                 println!("Error: {}", e);
